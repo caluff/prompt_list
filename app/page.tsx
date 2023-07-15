61452ef46726c63dfe86caed6312edfad02c9d83
Caluff <dcaluff@gmail.com>
2023-07-14 21:24:44 -0400

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-      <h1>
-        hello world
-      </h1>
+      <section className={"w-full flex-center flex-col"}>
+        <h1 className={"mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center"}>
+          Discover & Share &nbsp;
+          <br className={"max-md:hidden"}/>
+          <span className={"bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent text-center"}>
+            AI-Powered Prompts
+          </span>
+        </h1>
+        <p className={"mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl text-center"}>
+          Prompts List is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        </p>
+      </section>
   )
 }
